74382526ae23e538ac20cbba5dad77069a84c28a
Ryan Wise <ryanmwise1@gmail.com>
2017-03-23 07:11:23 -0700

    WIP: code cleanup.

--- game_agent_utils_test.py
+++ game_agent_utils_test.py
@@ -33,15 +33,21 @@ class SymmetryTest(unittest.TestCase):
 
             # Tesing equivalences -- e.g., that rotating 90 degrees and then another 90 degress
             # is the same as rotating 180 degrees in one go.
-            self.assertEqual(sym.rotate_180(loc, w, h), sym.rotate_90(sym.rotate_90(loc, w, h), w, h))
-            self.assertEqual(sym.rotate_270(loc, w, h), sym.rotate_180(sym.rotate_90(loc, w, h), w, h))
-            self.assertEqual(sym.rotate_270(loc, w, h), sym.rotate_90(sym.rotate_180(loc, w, h), w, h))
-            self.assertEqual(sym.rotate_270(loc, w, h), sym.rotate_90(sym.rotate_90(sym.rotate_90(loc, w, h), w, h), w, h))
-            self.assertEqual(sym.rotate_180(loc, w, h), sym.reflect_vertical(sym.reflect_horizontal(loc, w, h), w, h))
+            self.assertEqual(sym.rotate_180(loc, w, h), sym.rotate_90(
+                                                          sym.rotate_90(loc, w, h), w, h))
+            self.assertEqual(sym.rotate_270(loc, w, h), sym.rotate_180(
+                                                          sym.rotate_90(loc, w, h), w, h))
+            self.assertEqual(sym.rotate_270(loc, w, h), sym.rotate_90(
+                                                          sym.rotate_180(loc, w, h), w, h))
+            self.assertEqual(sym.rotate_270(loc, w, h), sym.rotate_90(
+                                                          sym.rotate_90(
+                                                            sym.rotate_90(loc, w, h), w, h), w, h))
+            self.assertEqual(sym.rotate_180(loc, w, h), sym.reflect_vertical(
+                                                          sym.reflect_horizontal(loc, w, h), w, h))
             self.assertEqual(sym.rotate_90(loc, w, h), sym.reflect_vertical(
-                                                        sym.reflect_horizontal(
-                                                         sym.reflect_vertical(
-                                                          sym.reflect_secondary_diagonal(loc, w, h), w, h), w, h), w, h))
+                                                         sym.reflect_horizontal(
+                                                           sym.reflect_vertical(
+                                                             sym.reflect_secondary_diagonal(loc, w, h), w, h), w, h), w, h))
 
 
 if __name__ == '__main__':
