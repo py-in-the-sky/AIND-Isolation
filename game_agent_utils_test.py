93b81c3346f182246b509837835f36117e52e872
Ryan Wise <ryanmwise1@gmail.com>
2017-03-23 07:06:53 -0700

    WIP: code cleanup.

--- game_agent_utils_test.py
+++ game_agent_utils_test.py
@@ -11,6 +11,8 @@ class SymmetryTest(unittest.TestCase):
             w, h = randint(1, 15), randint(1, 15)  # Rectangular grid.
             r, c = randrange(0, h), randrange(0, w)
             loc = (r, c)
+
+            # Testing that complementary actions undo each other.
             self.assertEqual(loc, sym.reflect_vertical(sym.reflect_vertical(loc, w, h), w, h))
             self.assertEqual(loc, sym.reflect_horizontal(sym.reflect_horizontal(loc, w, h), w, h))
             self.assertEqual(loc, sym.rotate_180(sym.rotate_180(loc, w, h), w, h))
@@ -19,6 +21,8 @@ class SymmetryTest(unittest.TestCase):
             h = w = randint(1, 15)  # Square grid.
             r, c = randrange(0, h), randrange(0, w)
             loc = (r, c)
+
+            # Testing that complementary actions undo each other.
             self.assertEqual(loc, sym.reflect_vertical(sym.reflect_vertical(loc, w, h), w, h))
             self.assertEqual(loc, sym.reflect_horizontal(sym.reflect_horizontal(loc, w, h), w, h))
             self.assertEqual(loc, sym.reflect_secondary_diagonal(sym.reflect_secondary_diagonal(loc, w, h), w, h))
@@ -26,6 +30,18 @@ class SymmetryTest(unittest.TestCase):
             self.assertEqual(loc, sym.rotate_270(sym.rotate_90(loc, w, h), w, h))
             self.assertEqual(loc, sym.rotate_180(sym.rotate_180(loc, w, h), w, h))
             self.assertEqual(loc, sym.rotate_270(sym.rotate_90(loc, w, h), w, h))
+
+            # Tesing equivalences -- e.g., that rotating 90 degrees and then another 90 degress
+            # is the same as rotating 180 degrees in one go.
+            self.assertEqual(sym.rotate_180(loc, w, h), sym.rotate_90(sym.rotate_90(loc, w, h), w, h))
+            self.assertEqual(sym.rotate_270(loc, w, h), sym.rotate_180(sym.rotate_90(loc, w, h), w, h))
+            self.assertEqual(sym.rotate_270(loc, w, h), sym.rotate_90(sym.rotate_180(loc, w, h), w, h))
+            self.assertEqual(sym.rotate_270(loc, w, h), sym.rotate_90(sym.rotate_90(sym.rotate_90(loc, w, h), w, h), w, h))
+            self.assertEqual(sym.rotate_180(loc, w, h), sym.reflect_vertical(sym.reflect_horizontal(loc, w, h), w, h))
+            self.assertEqual(sym.rotate_90(loc, w, h), sym.reflect_vertical(
+                                                        sym.reflect_horizontal(
+                                                         sym.reflect_vertical(
+                                                          sym.reflect_secondary_diagonal(loc, w, h), w, h), w, h), w, h))
 
 
 if __name__ == '__main__':
